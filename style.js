d90bbd64fc8675a6d9ac5d5ec986735124b1cf77
kanth3010 <rshrikanth777@gmail.com>
2021-07-02 19:09:21 +0530

    first commit

---
+++ style.js
@@ -0,0 +1,11 @@
+function colour(x) {
+  if (x==0){
+  document.getElementById('zero').style.backgroundColor = '#A2DBFA' ;
+}
+  else if (x==1){
+    document.getElementById('first').style.backgroundColor = '#A2DBFA' ;
+  }
+  else if (x==2){
+    document.getElementById('second').style.backgroundColor = '#A2DBFA' ;
+  }
+}
